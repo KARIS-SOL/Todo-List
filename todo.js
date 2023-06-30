874c43a5eee4bd431c902713e38337acfe0fc4f1
JIN SOL BAIK <121254493+KARIS-SOL@users.noreply.github.com>
2023-06-30 22:47:42 +0900

    Delete Button 추가

--- todo.js
+++ todo.js
@@ -11,7 +11,7 @@ const getAllTodos = () => {
   return todos;
 };
 
-const appendTodos = (text) => {
+const appendTodos = () => {
   const newId = id++;
   const newTodos = getAllTodos().concat({
     id: newId,
@@ -23,11 +23,41 @@ const appendTodos = (text) => {
 };
 
 const paintTodos = () => {
-  todoListElem.innerHTML = ""; //todoListElem 요소안의 HTML 초기화
-  const allTodos = getAllTodos(); // todos 배열가져오기
+  todoListElem.innerHTML = ""; //todoListElem 요소 안의 HTML 초기화
+  const allTodos = getAllTodos(); // todos 배열 가져오기
 
-  // 'todo-item' 에 해당하는 HTML을 그려서 'todo-list '에 추가하기
+  allTodos.forEach((todo) => {
+    const todoItemElem = document.createElement("li");
+    todoItemElem.classList.add("todo-item");
+
+    todoItemElem.setAttribute("data-id", todo.id);
+
+    const checkboxElem = document.createElement("div");
+    checkboxElem.classList.add("checkbox");
+    checkboxElem.addEventListener("click", () => completeTodo(todo.id));
+
+    const todoElem = document.createElement("div");
+    todoElem.classList.add("todo");
+    todoElem.innerText = todo.content;
+
+    const delBtnElem = document.createElement("button");
+    delBtnElem.classList.add("delBtn");
+    delBtnElem.addEventListener("click", () => deleteTodo(todo.id));
+    delBtnElem.innerHTML = "X";
+
+    if (todo.isCompleted) {
+      todoItemElem.classList.add("checked");
+      checkboxElem.innerText = "✔";
+    }
+
+    todoItemElem.appendChild(checkboxElem);
+    todoItemElem.appendChild(todoElem);
+    todoItemElem.appendChild(delBtnElem);
+
+    todoListElem.appendChild(todoItemElem);
+  });
 };
+
 const init = () => {
   todoInputElem.addEventListener("keypress", (e) => {
     if (e.key === "Enter") {
@@ -38,3 +68,5 @@ const init = () => {
 };
 
 init();
+
+// 'todo-item' 에 해당하는 HTML을 그려서 'todo-list '에 추가하기
