65da1cc2e4cd37147ae1a2dcf49114dd4db836e8
JIN SOL BAIK <121254493+KARIS-SOL@users.noreply.github.com>
2023-07-05 23:16:46 +0900

    수정

--- todo.js
+++ todo.js
@@ -26,6 +26,7 @@ const paintTodos = () => {
   todoListElem.innerHTML = ""; //todoListElem 요소 안의 HTML 초기화
   const allTodos = getAllTodos(); // todos 배열 가져오기
 
+  // "todo-item" 에 해당하는 HTML 을 그려서 "todo-list"에 추가하기
   allTodos.forEach((todo) => {
     const todoItemElem = document.createElement("li");
     todoItemElem.classList.add("todo-item");
@@ -49,12 +50,6 @@ const paintTodos = () => {
       todoItemElem.classList.add("checked");
       checkboxElem.innerText = "✔";
     }
-
-    todoItemElem.appendChild(checkboxElem);
-    todoItemElem.appendChild(todoElem);
-    todoItemElem.appendChild(delBtnElem);
-
-    todoListElem.appendChild(todoItemElem);
   });
 };
 
